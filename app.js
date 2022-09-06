69f9830f6ff87502275b13c4f05e46ade9f1b170
bimaagung <bimaagung.labs@gmail.com>
2022-09-06 20:13:33 +0700

    add endpoint date

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
 app.get('/greet/:name', (req, res) => {
   res.send('Hello ' + req.params['name'])
+})
+
+app.get('/date', (req, res) => {
+  res.send(new Date())
 })
 
 app.listen(port, () => {
